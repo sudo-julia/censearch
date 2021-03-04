d3891290310ef51c3bf50f34ab0e68ff4fdbb689
sudo-julia <jlearning@tuta.io>
2021-03-04 11:33:40 -0800

    config added

--- censearch/__init__.py
+++ censearch/__init__.py
@@ -1,11 +1,36 @@
-from pathlib import Path
+from __future__ import annotations
+import configparser
+import os
 
-key_dir = Path("~/lib/api_keys/twitter/censearch").expanduser()
 
-with open(f"{key_dir}/consumer_key") as f:
-    consumer_key: str = f.read().strip()
+def get_config() -> list[str]:
+    """get options from config file"""
+    config = configparser.ConfigParser()
+    config.read("config.ini")
 
-with open(f"{key_dir}/consumer_secret") as f:
-    consumer_secret: str = f.read().strip()
+    api_keys: list[str] = []
+    api_keys.append(config["KEYS"]["ConsumerKey"])
+    api_keys.append(config["KEYS"]["ConsumerSecret"])
 
-VERSION = "0.1.1"
+    # expand tilde to home path
+    for key in api_keys:
+        if key[0] == "~":
+            api_keys[api_keys.index(key)] = os.path.expanduser(key)
+
+    return api_keys
+
+
+def read_keys(location: str) -> str:
+    """get key values from locations"""
+    with open(location) as file:
+        value: str = file.read().strip()
+    return value
+
+
+# unload the keys to variable names for readability in main module
+keys = get_config()
+consumer_key: str = read_keys(keys[0])
+consumer_secret: str = read_keys(keys[1])
+del keys  # free up memory from keys, as values are unpacked
+
+VERSION: str = "0.1.1"
