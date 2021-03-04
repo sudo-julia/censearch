1290ebd73c52932c7a8333e27f193f8308c49a67
sudo-julia <jlearning@tuta.io>
2021-03-03 22:11:31 -0800

    it works

--- censearch/__main__.py
+++ censearch/__main__.py
@@ -7,11 +7,12 @@ from censearch import consumer_key, consumer_secret
 
 def get_variations(search_term: str) -> list[str]:
     """rotate a term through every iteration of
-    a letter being censored by an asterisk
+    a letter being censored by an asterisk.
+    the first and last letters are skipped, as those usually aren't censored
     """
-    total: int = len(search_term)
+    total: int = len(search_term) - 1
     variations: list[str] = []
-    for _ in range(total):
+    for _ in range(1, total):
         variations.append(search_term.replace(search_term[_], "*"))
     return variations
 
@@ -45,11 +46,22 @@ def main():
     search_term: str = args.search
     censored_words: list[str] = get_variations(search_term)
     tweet_list: list = get_tweets(censored_words)
+
+    block_num: int = 0
+    print("Gathering tweets...")
     for block in tweet_list:
-        num_tweets: int = len(block["statuses"])
         block = block["statuses"]
+        num_tweets: int = len(block)
+        print("-" * 80)
+        print(f"\nSearch phrase: '{censored_words[block_num]}'")
         for tweet in range(num_tweets):
-            print(f"Tweet:\n{block[tweet]['text']}\n")
+            author: str = block[tweet]["user"]["screen_name"]
+            tweet_id: str = block[tweet]["id_str"]
+            url: str = f"https://twitter.com/{author}/status/{tweet_id}"
+            print(f"\nTweet {tweet + 1}:\n{block[tweet]['text']} - @{author}")
+            print(f"{url}")
+        block_num += 1
+    print("-" * 80)
 
 
 if __name__ == "__main__":
